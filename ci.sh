f8fdaf71a8bf85c0bb11d88edc94e3680d2ffde8
MOZGIII <mike-n@narod.ru>
2021-04-12 21:26:06 +0300

    Run tests in ci

--- ci.sh
+++ ci.sh
@@ -1,10 +1,10 @@
-build_test(){
+build_test() {
   exit_code=0
   while read path; do
     printf "Project: %s\n" "$path"
     cargo build --verbose --manifest-path "$path" || exit_code=1
     #cargo test takes too much time to complete on small VM
-    #cargo test --verbose --manifest-path "$path" || exit_code=1
+    cargo test --verbose --manifest-path "$path" || exit_code=1
   done
   exit $exit_code
 }
